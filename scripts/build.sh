dd64845b6b86ca91b739d4f7f327e5baea8ffa3b
FigmentEngine <319349+FigmentEngine@users.noreply.github.com>
2025-11-11 10:25:40 +0000

    fix delimiter in sed

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Update humans.txt date
-sed -i "s/Last update: .*/Last update: $(date +%Y\/%-m\/%-d)/" src/humans.txt
+sed -i "s#Last update: .*#Last update: $(date +%Y/%m/%d)#" src/humans.txt
 
 # Install mdBook
 curl -sSL https://github.com/rust-lang/mdBook/releases/download/v0.4.40/mdbook-v0.4.40-x86_64-unknown-linux-gnu.tar.gz | tar -xz
